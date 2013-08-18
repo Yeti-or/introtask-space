09335b3b298f521181dcc0e674bd411857f84069
Yeti (rvv-64) <yeti.or@gmail.com>
2013-08-18 20:26:37 +0400

    Remove unsused spec.

--- test/spec/hello.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
-        expect(1).toEqual(1);
-    });
-});
