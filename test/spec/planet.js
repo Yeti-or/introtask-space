e17ce1ea37cb1adeee35de7a1c6507b41f5a15bc
Yeti <Yeti.or@gmail.com>
2013-08-18 13:26:12 +0400

    Add specs to planet creation.

--- test/spec/planet.js
+++ test/spec/planet.js
@@ -4,7 +4,22 @@ describe("A planet", function() {
     describe("Planet creation",function(){
         it("should create planet with name, position & amountOfCargo", function() {
             var mars = new Planet('Mars',[0,0],1000);
+            expect(mars).toBeDefined();
             expect(mars.name).toBe('Mars');
+            expect(mars.position).toBe([0,0]);
+            expect(mars.cargo).toBe(1000);
+        });
+        it("should create Planet only with 3 params",function(){
+            expect(new Planet('Mars',[0,0])).toThrow();
+            expect(new Planet('Mars')).toThrow();
+            expect(new Planet()).toThrow();
+        });
+        it("should have two coordinates",function(){
+            expect(new Planet('Mars',[0],1000)).toThrow();
+            expect(new Planet('Mars',[0,1,2],1000)).toThrow();
+        });
+        it("should have positive cargo",function(){
+            expect(new Planet('Mars',[0,0],-1000)).toThrow();
         });
     });
 });
